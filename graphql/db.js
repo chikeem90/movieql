57aad2e2fdc299082ee416376fed4b2d490b0bb0
chikeem90 <chikeem90@gmail.com>
2021-03-26 01:34:13 +0900

    #0.13 Wrapping a REST API with GraphQL Part Two

--- graphql/db.js
+++ graphql/db.js
@@ -1,54 +1,16 @@
-let movies = [
-    {
-        id: 0,
-        name: "Star Wars - The new one",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Avengers - The new one",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "The Godfather I",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "Logan",
-        score: 2
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.am/api/v2/list_movies.json?"
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if (limit > 0) {
+        REQUEST_URL += `limit=${limit}`;
     }
-];
-
-// movies를 반환하는 함수
-export const getMovies = () => movies;
-
-// id에 해당하는 movie를 반환하는 함수
-export const getById = (id) => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-// id에 해당하는 movie를 제외하는 함수
-export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    // 해당하는 id의 movie가 있어서 제외된 경우
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
+    if (rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
-}
-
-// add Movie
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
+    return fetch(REQUEST_URL)
+            .then(res => res.json())
+            .then(json => json.data.movies);
 }
